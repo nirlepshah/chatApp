4ca1e395f9b09d3e30b780dfefa715c04fdf328e
nirlepshah <nirlepshah@gmail.com>
2022-03-05 12:06:54 -0500

    Add comments in Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -5,8 +5,10 @@ import { View, Text, Button } from 'react-native';
 export default class Chat extends React.Component {
 
     render() {
+        // Value received from the Start screen. Entered nams is displayed on the top 
         let name = this.props.route.params.name;
         this.props.navigation.setOptions({ title: name })
+        // Background color received from the Start screen 
         const { bgColor } = this.props.route.params;
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: bgColor }}>
